9c13ffecdd84c534129d989d60469930a4c730ff
Fernando Karnagi <fernando@cyder.com.sg>
2017-09-16 06:49:21 +0800

    make it better

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var home = require('./routes/home');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -20,10 +20,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+var static_options = {
+  dotfiles: 'ignore',
+  etag: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-caralla-timestamp', Date.now())
+  }
+}
+
+app.use('/static', express.static(path.join(__dirname, 'public'), static_options));
+
+app.use('/', home);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
