68b632ee717ad0538b2bd5394fe0ed604c388760
Fernando Karnagi <fernando@cyder.com.sg>
2017-09-16 18:41:43 +0800

    make it better

--- routes/buycar.js
+++ routes/buycar.js
@@ -5,6 +5,18 @@ var router = express.Router();
 var carallaSiteService = new site();
 
 /* GET home page. */
+
+router.post('/result', function(req, res, next) {
+  var renderVars = carallaSiteService.global;
+  res.render('buycar/searchresult', renderVars);
+});
+
+router.get('/result', function(req, res, next) {
+  var renderVars = carallaSiteService.global;
+  res.render('buycar/searchresult', renderVars);
+});
+ 
+
 router.get('/', function(req, res, next) {
   var renderVars = carallaSiteService.global;
   res.render('buycar/buycar', renderVars);
